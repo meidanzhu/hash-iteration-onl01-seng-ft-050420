d62bc64864ab6c528b1a1c877316fa2881c42fd0
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-07 17:41:21 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -23,7 +23,7 @@ end
 def age_appropriate_birthday (birthday_kids, age)
   birthday_kids.each do |name, age|
     if age <= 12
-       return "Happy Birthday #{birthday_kids}"
+       return "Happy Birthday #{birthday_kids}! You are now #{age} years old!"
     end
   end
 
